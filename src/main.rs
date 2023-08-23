6c374e283cbcec2fade22f936ddeed81d210b291
Ilya <marhoily@hotmail.com>
2023-08-23 19:54:27 +0300

    problem 3

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,69 @@
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 use reqwest::header::COOKIE;
 use std::{error::Error, str::FromStr};
 
 #[derive(Debug, Clone, Copy)]
 struct Round {
-    you: Move,
-    opponent: Move,
-}
-impl Round {
-    fn score(&self) -> u8 {
-        let diff = self.you as i32 - self.opponent as i32;
-        let outcome = diff.rem_euclid(3);
-     
-        let outcome_score = match outcome {
-            0 => 3,
-            1 => 6,
-            _ => 0,
-        };
-        println!("{:?} {:?}", outcome_score, self.you as u8);
-        outcome_score + self.you as u8
-    }
+    opponents_move: Move,
+    desired_outcome: Outcome,
 }
 #[derive(Debug, Clone, Copy)]
+enum Outcome {
+    Win = 1,
+    Loss = 2,
+    Draw = 0,
+}
+impl Outcome {
+    fn score(self: Outcome) -> i32 {
+        match self {
+            Outcome::Loss => 0,
+            Outcome::Draw => 3,
+            Outcome::Win => 6,
+        }
+    }
+    fn parse(c: char) -> Option<Self> {
+        match c {
+            'X' => Some(Outcome::Loss),
+            'Y' => Some(Outcome::Draw),
+            'Z' => Some(Outcome::Win),
+            _ => None,
+        }
+    }
+}
+impl Round {
+    fn score(&self) -> i32 {
+        let your_move = self.opponents_move
+            .find_move_for(self.desired_outcome);
+        println!("Opponent's move: {:?}, Desired Outcome: {:?}, Your move: {:?}", 
+            self.opponents_move, self.desired_outcome, your_move);
+        your_move.score() + self.desired_outcome.score()
+    }
+}
+#[derive(Debug, Clone, Copy, FromPrimitive)]
 enum Move {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
+}
+impl Move {
+    fn score(self: Move) -> i32 {
+        self as i32
+    }
+    fn find_move_for(self: Move, desired_outcome: Outcome) -> Move {
+        let diff = self as i32 - 1 + desired_outcome as i32;
+        let reminder = diff.rem_euclid(3) + 1;
+        Move::from_i32(reminder)
+            .expect(format!("ERR: diff: {:?}; reminder: {:?}", diff, reminder).as_str())
+    }
+    fn parse(c: char) -> Option<Self> {
+        match c {
+            'A' => Some(Move::Rock),
+            'B' => Some(Move::Paper),
+            'C' => Some(Move::Scissors),
+            _ => None,
+        }
+    }
 }
 #[derive(Debug)]
 struct Script(Vec<Round>);
@@ -45,27 +84,24 @@ impl FromStr for Script {
         Ok(Script(result))
     }
 }
+
 impl FromStr for Round {
     type Err = &'static str;
 
     fn from_str(line: &str) -> Result<Self, Self::Err> {
-        let x = match line.chars().nth(0) {
-            Some('A') => Some(Move::Rock),
-            Some('B') => Some(Move::Paper),
-            Some('C') => Some(Move::Scissors),
-            _ => None,
-        }
-        .ok_or("first value in line is not found")?;
-        let y = match line.chars().nth(2) {
-            Some('X') => Some(Move::Rock),
-            Some('Y') => Some(Move::Paper),
-            Some('Z') => Some(Move::Scissors),
-            _ => None,
-        }
-        .ok_or("second value in line is not found")?;
+        let x = line
+            .chars()
+            .nth(0)
+            .map_or(None, Move::parse)
+            .ok_or("first value in line is not found")?;
+        let y = line
+            .chars()
+            .nth(2)
+            .map_or(None, Outcome::parse)
+            .ok_or("second value in line is not found")?;
         Ok(Round {
-            you: y,
-            opponent: x,
+            opponents_move: x,
+            desired_outcome: y,
         })
     }
 }
